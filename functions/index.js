9082fc29fe76c16ac6c936c72ef64032534b28aa
bjorn <blmhansen@gmail.com>
2022-03-31 23:11:39 -0700

    Lots of Stripe work

--- functions/index.js
+++ functions/index.js
@@ -3,6 +3,7 @@ const functions = require('firebase-functions');
 
 // The Firebase Admin SDK to access Firestore.
 const admin = require('firebase-admin');
+const {user} = require("firebase-functions/v1/auth");
 admin.initializeApp();
 
 // // Create and Deploy Your First Cloud Functions
@@ -15,29 +16,28 @@ admin.initializeApp();
 
 // Take the text parameter passed to this HTTP endpoint and insert it into
 // Firestore under the path /messages/:documentId/original
-exports.setupStripe = functions.https.onCall(async (data, context) => {
+exports.stripeStart = functions.https.onCall(async (data, context) => {
     try {
         // Set your secret key. Remember to switch to your live secret key in production.
         // See your keys here: https://dashboard.stripe.com/apikeys
-        const stripe = require('stripe')('sk_test_51KWRApBLF6OO4ModlnDOzAe9TzKbqXwBd1g9Mmp7K36JaFog1WQb78K55Gb00J34QLtU8TsqRNxu3grtYUN4yt1J00W0p3W9QN');
+        const stripe = require('stripe')(process.env.STRIPE_SECRET);
 
-        // Does this user have a Stripe ID? If so, we don't need to create a new Customer for them.
+        // Get the user data.
         const userDoc = await admin.firestore().collection('users').doc(context.auth.uid).get();
         if (!userDoc.exists) {
             // Shouldn't be able to get here. But if we do, throw an error.
         }
-
-        // Grab the data from the snapshot.
         const userData = userDoc.data();
 
-        // Add up the cause amounts and store it so we can charge that price.
+        // Add up the cause amounts and store it, so we can charge that price.
         let price = 0;
         for (const c of userData.causes) {
             price += c.giving_amount;
         }
 
+        // Does this user already have a Stripe Customer ID?
         if (userData.stripe_customer_id) {
-            // User is already a Customer. Do they have a subscription?
+            // Yes. User is already a Customer. Do they have a subscription?
             const subscriptions = await stripe.subscriptions.list({
                 customer: userData.stripe_customer_id,
                 limit: 10
@@ -45,9 +45,7 @@ exports.setupStripe = functions.https.onCall(async (data, context) => {
             if (subscriptions.length) {
                 // The user already has at least one active subscription.
                 // We don't need to do anything else here. Tell the client that all is well.
-                return {
-                    existingSubscription: true
-                }
+                return {status: 'existing_customer_with_active_subscription'};
             } else {
                 // No active subscriptions. Does the user have a working payment method?
                 const paymentMethods = await stripe.paymentMethods.list({
@@ -56,15 +54,17 @@ exports.setupStripe = functions.https.onCall(async (data, context) => {
                 });
                 if (paymentMethods.data.length) {
                     // The user has at least one valid payment method. We can go ahead and start a subscription.
+                    return {status: 'existing_customer_without_active_subscription_but_with_payment_method'};
                 } else {
                     // No payment methods. We'll need to add one.
+                    return {status: 'existing_customer_without_active_subscription_and_without_payment_method'};
                 }
             }
         } else {
             // Not a Customer yet. Create a new one in Stripe.
             const customer = await stripe.customers.create({
-                email: data.email,
-                name: data.name
+                email: userData.email,
+                name: `${userData.first_name} ${userData.last_name}`
             });
 
             // Store the Customer ID in the user doc.
@@ -80,7 +80,7 @@ exports.setupStripe = functions.https.onCall(async (data, context) => {
                         currency: 'USD',
                         product: 'prod_LPLotWTxSn8Fxm',
                         recurring: {interval: 'month'},
-                        unit_amount: price
+                        unit_amount: price * 100 // Convert to cents
                     }
                 }],
                 payment_behavior: 'default_incomplete',
@@ -89,11 +89,13 @@ exports.setupStripe = functions.https.onCall(async (data, context) => {
 
             // Give the data to the client.
             return {
-                subscriptionId: subscription.id,
                 clientSecret: subscription.latest_invoice.payment_intent.client_secret,
             };
         }
     } catch (error) {
-        return error;
+        return {
+            isError: true,
+            message: error.message
+        };
     }
 });
